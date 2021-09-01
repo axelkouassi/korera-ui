764e32ec459494e8cde309501b05525669aa1b9b
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-01 17:42:30 -0400

    Registration form validation with bootstrap.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Container fluid>
       <Row>
@@ -12,30 +25,45 @@ const Register = () => {
           <br />
         </Col>
       </Row>
-      <Form>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group controlId="formBasicFirstName">
           <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter first name" />
+          <Form.Control required type="text" placeholder="Enter first name" />
+          <Form.Control.Feedback type="invalid">
+            First name is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <br />
         <Form.Group controlId="formBasicLastName">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter last name" />
+          <Form.Control required type="text" placeholder="Enter last name" />
+          <Form.Control.Feedback type="invalid">
+            Last name is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <br />
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
+          <Form.Control required type="text" placeholder="Enter username" />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <br />
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control required type="email" placeholder="Enter email" />
+          <Form.Control.Feedback type="invalid">
+            Valid email is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <br />
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control required type="password" placeholder="Password" />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
         </Form.Group>
         <br />
 
