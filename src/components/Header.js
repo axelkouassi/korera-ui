78d0ce4f1521bcdcda70b490bc90d3b88c7a904a
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-01 19:34:56 -0400

    Styling navbar with brand image and other menus.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,45 @@
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
 const Header = () => {
   return (
-    <Container fluid>
-      <Row>
-        <header className="App-header">
-          <nav>
-            <ul>
-              <Col>
-                <li>
-                  <Link to="/">Resource Management</Link>
-                </li>
-              </Col>
-              <Col>
-                <li>
-                  <Link to="/">Icon Login</Link>
-                </li>
-              </Col>
-              <Col>
-                <li>
-                  <Link>Icon Project Description</Link>
-                </li>
-              </Col>
-            </ul>
-          </nav>
-        </header>
-      </Row>
-    </Container>
+    // <header className="App-header">
+    <Navbar
+      bg="light"
+      /* fixed="top" */
+      justify-content-between
+      expand="lg"
+    >
+      <Navbar.Brand>
+        <Link to="/">
+          <img
+            src="/Capture.JPG"
+            fluid
+            className="d-inline-block align-top"
+            alt="Resource Management"
+            width="200"
+            height="200"
+            style={({ maxWidth: "100%" }, { width: "50%" }, { height: "auto" })}
+          />
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto my-2 my-lg-0 ">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Icon Login</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Icon Project Description</Link>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+    //</header> */}
   );
 };
 
