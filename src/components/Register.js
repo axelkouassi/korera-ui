a08e7e385bf2b03ff2c0fac9fe5f48c8217c0a8c
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-01 19:36:38 -0400

    Updating controlId to match with form labels.

--- src/components/Register.js
+++ src/components/Register.js
@@ -16,86 +16,88 @@ const Register = () => {
   };
 
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <div id="register">
-            <h1>Register</h1>
-          </div>
-          <br />
-        </Col>
-      </Row>
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control required type="text" placeholder="Enter first name" />
-          <Form.Control.Feedback type="invalid">
-            First name is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <br />
-        <Form.Group controlId="formBasicLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control required type="text" placeholder="Enter last name" />
-          <Form.Control.Feedback type="invalid">
-            Last name is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <br />
-        <Form.Group controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control required type="text" placeholder="Enter username" />
-          <Form.Control.Feedback type="invalid">
-            Username is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <br />
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control required type="email" placeholder="Enter email" />
-          <Form.Control.Feedback type="invalid">
-            Valid email is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <br />
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control required type="password" placeholder="Password" />
-          <Form.Control.Feedback type="invalid">
-            Password is required!
-          </Form.Control.Feedback>
-        </Form.Group>
-        <br />
-
-        <Form.Group controlId="formBasicMobileNumber">
-          <Form.Label>Mobile Number</Form.Label>
-          <Form.Control type="text" placeholder="Enter mobile number" />
-        </Form.Group>
-        <br />
-        <Button className="btn-lg btn-block" variant="primary" type="submit">
-          Register
-        </Button>
-        <br />
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Remember me" />
-        </Form.Group>
-        <br />
+    <div>
+      <Container fluid>
         <Row>
           <Col>
-            <Link to="/">
-              <Button variant="primary" type="submit">
-                Login
-              </Button>
-            </Link>
-          </Col>
-          <Col>
-            <Form.Text className="text-muted">
-              <Link to="/">Already have an account?</Link>
-            </Form.Text>
+            <div id="register">
+              <h1>Register</h1>
+            </div>
+            <br />
           </Col>
         </Row>
-      </Form>
-    </Container>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group hasValidation controlId="formBasicFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control required type="text" placeholder="Enter first name" />
+            <Form.Control.Feedback type="invalid">
+              First name is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br />
+          <Form.Group hasValidation controlId="formBasicLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control required type="text" placeholder="Enter last name" />
+            <Form.Control.Feedback type="invalid">
+              Last name is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br />
+          <Form.Group hasValidation controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control required type="text" placeholder="Enter username" />
+            <Form.Control.Feedback type="invalid">
+              Username is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br />
+          <Form.Group hasValidation controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control required type="email" placeholder="Enter email" />
+            <Form.Control.Feedback type="invalid">
+              Valid email is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br />
+          <Form.Group hasValidation controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control required type="password" placeholder="Password" />
+            <Form.Control.Feedback type="invalid">
+              Password is required!
+            </Form.Control.Feedback>
+          </Form.Group>
+          <br />
+
+          <Form.Group hasValidation controlId="formBasicMobileNumber">
+            <Form.Label>Mobile Number</Form.Label>
+            <Form.Control type="text" placeholder="Enter mobile number" />
+          </Form.Group>
+          <br />
+          <Button className="btn-lg btn-block" variant="primary" type="submit">
+            Register
+          </Button>
+          <br />
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <br />
+          <Row>
+            <Col>
+              <Link to="/">
+                <Button variant="primary" type="submit">
+                  Login
+                </Button>
+              </Link>
+            </Col>
+            <Col>
+              <Form.Text className="text-muted">
+                <Link to="/">Already have an account?</Link>
+              </Form.Text>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
   );
 };
 
