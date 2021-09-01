caeec0aa1d8f4e00ff3a27cf4afc3d91ee7402be
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-01 17:31:25 -0400

    Adding field validation to login form.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
     <Container fluid>
       <div id="login">
@@ -11,18 +24,21 @@ const Login = () => {
             <br />
           </Col>
         </Row>
-        <Form>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="Enter username" />
-            {/* <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text> */}
+            <Form.Control required type="text" placeholder="Enter username" />
+            <Form.Control.Feedback type="invalid">
+              Username is required!
+            </Form.Control.Feedback>
           </Form.Group>
           <br />
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control required type="password" placeholder="Password" />
+            <Form.Control.Feedback type="invalid">
+              Password is required!
+            </Form.Control.Feedback>
           </Form.Group>
           <br />
           <Button className="btn-lg btn-block" variant="primary" type="submit">
