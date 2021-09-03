3273e9a6cbf3090f32ed16cb6e6029b6188e45d7
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-03 05:48:29 -0400

    Axios get request generates 400 error, request body is missing.

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,13 +1,69 @@
 import { Col, Container, Row } from "react-bootstrap";
 import LoginForm from "./LoginForm";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 console.log("***Login Page***");
 console.log("Entering login page...");
 
-const LoginPage = (props) => {
+const LoginPage = () => {
   console.log("Inside login page");
 
-  const loginHandler = () => {};
+  const history = useHistory();
+
+  const getRequest = (loginInfo) => {
+    console.log("Inside get request");
+    /* const api = axios.create({
+      baseURL: "http://localhost:8080/korera/user",
+    }); */
+
+    console.log("Login info: ", loginInfo);
+
+    const loginData = loginInfo;
+    console.log("Inside axios get request");
+
+    // const axios = require("axios");
+
+    axios.defaults.baseURL = "http://localhost:8080/korera/user";
+
+    axios
+      .get("/login", loginData, {
+        headers: {
+          "content-type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        },
+      })
+      .then((res) => {
+        console.log("Login response: ", res);
+        console.log("Login data: ", res.data);
+        history.replace("/register");
+      })
+      .catch(function (error) {
+        // handle error
+        console.log("Inside catch");
+        console.log("Login data: ", loginData);
+        // console.log("Error message: ", error.message);
+        console.log("Error response data: ", error.response.data);
+        console.log(
+          "Error response data message: ",
+          error.response.data.message
+        );
+        console.log("Error caught: ", error);
+        /* console.log("Error response status: ", error.response.status);
+        console.log("Error response headers: ", error.response.headers); */
+      })
+      .then(function () {
+        // always executed
+        console.log("Inside then - always executed");
+      });
+  };
+
+  const loginHandler = (loginInfo) => {
+    console.log("Inside login handler");
+    console.log("Login info: ", loginInfo);
+    getRequest(loginInfo);
+  };
 
   return (
     <section>
