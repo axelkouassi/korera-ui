22efccd572078423d4cdfacd04e1ed3e98ebcf0b
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-02 22:34:58 -0400

    Colecting user's input data and storing inside one object that will be sent to the backend.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,46 @@
+import { useRef } from "react";
 import { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+console.log("Entering Register...");
+
 const Register = () => {
+  console.log("Inside  Register");
+
   const [validated, setValidated] = useState(false);
 
+  const usernameRef = useRef(); // username ref
+  const emailRef = useRef(); // email ref
+  const passwordRef = useRef(); // password ref
+  const firstNameRef = useRef(); // first name ref
+  const lastNameRef = useRef(); // last name ref
+  const mobileNumberRef = useRef(); // username ref
+
   const handleSubmit = (event) => {
+    console.log("Inside  handleSubmit");
+
+    event.preventDefault();
+
+    const enteredFirstName = firstNameRef.current.value; // user's entered first name
+    const enteredLastName = lastNameRef.current.value; // user's entered last name
+    const enteredUsername = usernameRef.current.value; // user's entered username
+    const enteredEmail = emailRef.current.value; // user's entered email
+    const enteredPassword = passwordRef.current.value; // user's entered password
+    const enteredMobileNumber = mobileNumberRef.current.value; // user's entered mobile number
+
+    //Object to store all user's entered information
+    const UserInfo = {
+      username: enteredUsername,
+      email: enteredEmail,
+      password: enteredPassword,
+      firstName: enteredFirstName,
+      lastName: enteredLastName,
+      phone: enteredMobileNumber,
+    };
+
+    console.log("User info: ", UserInfo);
+
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
@@ -27,74 +62,79 @@ const Register = () => {
           </Col>
         </Row>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicFirstName"
-          >
+          <Form.Group class="form-floating" controlId="formBasicFirstName">
             <Form.Label>First Name</Form.Label>
-            <Form.Control required type="text" placeholder="Enter first name" />
+            <Form.Control
+              ref={firstNameRef}
+              required
+              type="text"
+              placeholder="Enter first name"
+            />
             <Form.Control.Feedback type="invalid">
               First name is required!
             </Form.Control.Feedback>
           </Form.Group>
           <br />
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicLastName"
-          >
+          <Form.Group class="form-floating" controlId="formBasicLastName">
             <Form.Label>Last Name</Form.Label>
-            <Form.Control required type="text" placeholder="Enter last name" />
+            <Form.Control
+              ref={lastNameRef}
+              required
+              type="text"
+              placeholder="Enter last name"
+            />
             <Form.Control.Feedback type="invalid">
               Last name is required!
             </Form.Control.Feedback>
           </Form.Group>
           <br />
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicUsername"
-          >
+          <Form.Group class="form-floating" controlId="formBasicUsername">
             <Form.Label>Username</Form.Label>
-            <Form.Control required type="text" placeholder="Enter username" />
+            <Form.Control
+              ref={usernameRef}
+              required
+              type="text"
+              placeholder="Enter username"
+            />
             <Form.Control.Feedback type="invalid">
               Username is required!
             </Form.Control.Feedback>
           </Form.Group>
           <br />
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicEmail"
-          >
+          <Form.Group class="form-floating" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control required type="email" placeholder="Enter email" />
+            <Form.Control
+              ref={emailRef}
+              required
+              type="email"
+              placeholder="Enter email"
+            />
             <Form.Control.Feedback type="invalid">
               Valid email is required!
             </Form.Control.Feedback>
           </Form.Group>
           <br />
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicPassword"
-          >
+          <Form.Group class="form-floating" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control required type="password" placeholder="Password" />
+            <Form.Control
+              ref={passwordRef}
+              required
+              type="password"
+              placeholder="Password"
+            />
             <Form.Control.Feedback type="invalid">
               Password is required!
             </Form.Control.Feedback>
           </Form.Group>
           <br />
 
-          <Form.Group
-            class="form-floating"
-            hasValidation
-            controlId="formBasicMobileNumber"
-          >
+          <Form.Group class="form-floating" controlId="formBasicMobileNumber">
             <Form.Label>Mobile Number</Form.Label>
-            <Form.Control type="text" placeholder="Enter mobile number" />
+            <Form.Control
+              ref={mobileNumberRef}
+              type="text"
+              placeholder="Enter mobile number"
+            />
           </Form.Group>
           <br />
           <Button className="btn-lg btn-block" variant="primary" type="submit">
