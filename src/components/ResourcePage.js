ba3818f8f05df5170f61691e688ad5b8417c1307
Kouassi Axel Geoffroy KOUASSI <ak659@njit.edu>
2021-09-06 04:27:03 -0400

    Drop down button to add row and column.

--- src/components/ResourcePage.js
+++ src/components/ResourcePage.js
@@ -1,9 +1,22 @@
-import Button from "@restart/ui/esm/Button";
 import React from "react";
-import { Col, Container, Form, FormControl, Row, Table } from "react-bootstrap";
 import {
-  PlusLg,
-  PlusSquare,
+  Col,
+  Container,
+  DropdownButton,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  Form,
+  FormControl,
+  Row,
+  Table,
+} from "react-bootstrap";
+import {
+  Columns,
+  Justify,
+  LayoutSplit,
+  List,
   PlusSquareFill,
   Search,
 } from "react-bootstrap-icons";
@@ -19,7 +32,7 @@ const ResourcePage = () => {
       <Table responsive striped bordered hover size="sm">
         <thead>
           <tr>
-            <th className="table-secondary" colSpan="2" fluid>
+            <th className="table-secondary fixed" colSpan="2" fluid>
               <Row>
                 <Col>
                   <Form className=" input-group w-50 d-flex">
@@ -29,25 +42,50 @@ const ResourcePage = () => {
                       placeholder="keyword"
                       //   className="mr-2"
                       aria-label="Search"
-                      aria-aria-describedby="basic-addon1"
+                      aria-labelledby="basic-addon1"
                     />
                     <span
                       className="input-group-text btn btn-secondary"
                       id="basic-addon1"
                       variant="outline-success"
+                      color="#ec641c"
+                      style={{ backgroundColor: "#ec641c" }}
                     >
-                      <Search className="bi bi-search" fill="currentColor" />
+                      <Search
+                        className="bi bi-search"
+                        fill="white"
+                        height="18"
+                        width="18"
+                        justify-content-between="true"
+                      />
                     </span>
                   </Form>
                 </Col>
                 <Col>Resource Catalog</Col>
                 <Col>
-                  <PlusSquareFill
-                    className="bi bi-plus-sqaure fill"
-                    fill="currentColor"
-                    width="22"
-                    height="22"
-                  />
+                  <Dropdown /* id="dropdown-basic-button" */>
+                    <Dropdown.Toggle variant="link" bsPrefix="p-0">
+                      <PlusSquareFill
+                        /* className="bi bi-plus-square fill" */
+                        fill="#ec641c"
+                        width="25"
+                        height="25"
+                        /* justify-content-between="true" */
+                        style={{
+                          backgroundColor: "white",
+                          borderRadius: "0px",
+                        }}
+                      />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#/row">
+                        <Justify height="20" width="20" /> Add Row
+                      </Dropdown.Item>
+                      <Dropdown.Item href="#/column">
+                        <LayoutSplit height="16" width="16" /> Add Column
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </Col>
               </Row>
             </th>
